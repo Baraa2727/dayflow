da1e7a67372bc80e115c1334509ccd9b39ac7e83
Baraa Shareet <b.shareet@phase5.de>
2025-10-21 16:30:03 +0200

    Failsafe: /api/home liefert index.html; Routing korrigiert

---
+++ api/home.js
@@ -0,0 +1,14 @@
+// /api/home.js – liefert index.html als HTML
+import fs from "fs";
+import path from "path";
+
+export default function handler(req, res) {
+  try {
+    const filePath = path.join(process.cwd(), "index.html");
+    const html = fs.readFileSync(filePath, "utf8");
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.status(200).send(html);
+  } catch (err) {
+    res.status(500).send(`<pre>Fehler: index.html nicht gefunden\n${err?.message || err}</pre>`);
+  }
+}
